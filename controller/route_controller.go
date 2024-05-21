6740e65f12b3b7301d35bae671896e0e2cf37880
Bharat Kathi <bharat1031@gmail.com>
2024-05-20 20:51:29 -0700

    add ping admin route

--- controller/route_controller.go
+++ controller/route_controller.go
@@ -1,1 +1,51 @@
 package controller
+
+import (
+	"encoding/base64"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"kerbecs/config"
+	"strings"
+	"time"
+)
+
+func SetupRouter() *gin.Engine {
+	if config.Env == "PROD" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		MaxAge:           12 * time.Hour,
+		AllowCredentials: true,
+	}))
+	r.Use(AuthMiddleware())
+	return r
+}
+
+func InitializeRoutes(router *gin.Engine) {
+	gw := router.Group("/admin-gw", func(c *gin.Context) {})
+	gw.GET("/ping", Ping)
+
+}
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if c.Request.Method == "POST" || c.Request.Method == "PUT" || c.Request.Method == "DELETE" {
+			auth := strings.SplitN(c.Request.Header.Get("Authorization"), " ", 2)
+			if len(auth) != 2 || auth[0] != "Basic" {
+				c.AbortWithStatusJSON(401, gin.H{"message": "Request not authorized"})
+				return
+			}
+			payload, _ := base64.StdEncoding.DecodeString(auth[1])
+			pair := strings.SplitN(string(payload), ":", 2)
+			if len(pair) != 2 || pair[0] != config.AuthUser || pair[1] != config.AuthPassword {
+				c.AbortWithStatusJSON(401, gin.H{"message": "Invalid credentials"})
+				return
+			}
+		}
+		c.Next()
+	}
+}
