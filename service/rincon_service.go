3c4de1b9f966f342be4ecfa5be3af8ea419d2db9
Bharat Kathi <bharat1031@gmail.com>
2024-05-23 10:55:39 -0700

    update rincon registration to check for endpoints

--- service/rincon_service.go
+++ service/rincon_service.go
@@ -7,23 +7,55 @@ import (
 )
 
 func RegisterRincon() {
+	rinconEndpoint := "http://rincon:" + config.Port
 	client, err := rincon.NewClient(rincon.Config{
-		BaseURL:           "http://localhost:10311",
+		BaseURL:           rinconEndpoint,
 		HeartbeatMode:     rincon.ServerHeartbeat,
 		HeartbeatInterval: 60,
 		AuthUser:          config.RinconUser,
 		AuthPassword:      config.RinconPassword,
 	})
 	if err != nil {
-		utils.SugarLogger.Errorf("Failed to create Rincon client: %v", err)
-		return
+		utils.SugarLogger.Errorf("Failed to create Rincon client with %s: %v", rinconEndpoint, err)
+		rinconEndpoint = "http://host.docker.internal:" + config.Port
+		client, err = rincon.NewClient(rincon.Config{
+			BaseURL:           rinconEndpoint,
+			HeartbeatMode:     rincon.ServerHeartbeat,
+			HeartbeatInterval: 60,
+			AuthUser:          config.RinconUser,
+			AuthPassword:      config.RinconPassword,
+		})
+		if err != nil {
+			utils.SugarLogger.Errorf("Failed to create Rincon client with %s: %v", rinconEndpoint, err)
+			rinconEndpoint = "http://localhost:" + config.Port
+			client, err = rincon.NewClient(rincon.Config{
+				BaseURL:           rinconEndpoint,
+				HeartbeatMode:     rincon.ServerHeartbeat,
+				HeartbeatInterval: 60,
+				AuthUser:          config.RinconUser,
+				AuthPassword:      config.RinconPassword,
+			})
+			if err != nil {
+				utils.SugarLogger.Errorf("Failed to create Rincon client with %s: %v", rinconEndpoint, err)
+				return
+			} else {
+				utils.SugarLogger.Infof("Created Rincon client with endpoint %s", rinconEndpoint)
+				utils.SugarLogger.Infof("Service is running on Host, Rincon running on Host!")
+			}
+		} else {
+			utils.SugarLogger.Infof("Created Rincon client with endpoint %s", rinconEndpoint)
+			utils.SugarLogger.Infof("Service is running on Docker, Rincon running on Host!")
+		}
+	} else {
+		utils.SugarLogger.Infof("Created Rincon client with endpoint %s", rinconEndpoint)
+		utils.SugarLogger.Infof("Service is running on Docker, Rincon running on Docker!")
 	}
 	config.RinconClient = client
 	config.Service = rincon.Service{
 		Name:        "Kerbecs",
 		Version:     config.Version,
-		Endpoint:    "http://localhost:" + config.AdminPort,
-		HealthCheck: "http://host.docker.internal:" + config.AdminPort + "/admin-gw/ping",
+		Endpoint:    "http://kerbecs:" + config.AdminPort,
+		HealthCheck: "http://kerbecs:" + config.AdminPort + "/admin-gw/ping",
 	}
 	id, err := config.RinconClient.Register(config.Service, []string{"/admin-gw/**"})
 	if err != nil {
